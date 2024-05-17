a4653a3d96c2d8bb19109a9ffb03262920513550
VIKAS KUMAR VERMA <vikassoftfix@gmail.com>
2024-05-17 12:12:38 +0530

    fix:some changes

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://103.174.103.122:3030/",
+    baseURL: "http://103.174.102.241:3030/",
   // baseURL: "http://127.0.0.1:3030/",
   headers: {
     post: {
