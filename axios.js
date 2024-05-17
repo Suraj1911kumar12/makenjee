f8a1bb1dfc9a70f5e8e7e7721f148430cc5858a9
VIKAS KUMAR VERMA <vikassoftfix@gmail.com>
2024-05-17 11:23:17 +0530

    fix: some changes

--- axios.js
+++ axios.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://103.174.103.122:3030/',
-    // baseURL: 'http://127.0.0.1:3030/',
-    headers: {
-        post: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
-        get: {
-            "Accept": 'application/json',
-            "Content-Type": "application/json",
-        },
+    baseURL: "http://103.174.103.122:3030/",
+  // baseURL: "http://127.0.0.1:3030/",
+  headers: {
+    post: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     get: {
       Accept: "application/json",
